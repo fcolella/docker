a744f918e835600a64e856a74b51cbd471b41d09
fcolella <fcolella@garbarino.com.ar>
2015-12-28 13:11:09 -0300

    CMS - Regions

--- resources/views/cms/regions/all.blade.php
+++ resources/views/cms/regions/all.blade.php
@@ -8,6 +8,8 @@
 				<tr>
 					<td>ID</td>
 					<td>Name</td>
+					<td>Countries</td>
+					<td>Options</td>
 				</tr>
 				</thead>
 				<tbody>
@@ -15,6 +17,11 @@
 					<tr>
 						<td>{{ $region->id }}</td>
 						<td>{{ $region->name }}</td>
+						<td>
+							@foreach($region->countries as $country)
+								{{ $country->name }}
+							@endforeach
+						</td>
 						<td>
 							<a class="btn btn-small btn-success" href="{{ URL::to(getenv('CMS_PATH').'/regions/'.$region->id) }}">Show Region</a>
 							<a class="btn btn-small btn-info" href="{{ URL::to(getenv('CMS_PATH').'/regions/'.$region->id.'/edit') }}">Edit Region</a>
