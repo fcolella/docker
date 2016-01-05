3593aa3d016ea2f63e17c8da469328e3dd60f008
fcolella <fcolella@garbarino.com.ar>
2016-01-05 10:24:38 -0300

    CMS - Regions less + order + scrollup plugin

--- resources/views/cms/layouts/header.blade.php
+++ resources/views/cms/layouts/header.blade.php
@@ -7,14 +7,24 @@
 	<link href="{{ asset('statics/cms/images/favicon.ico') }}" rel="shortcut icon">
     <title>Zeus CMS</title>
 
+	<link href="{{ asset('statics/cms/css/jquery-ui/1.11.4/jquery-ui.css') }}" rel="stylesheet">
+	<link href="{{ asset('statics/cms/css/jquery-ui/1.11.4/jquery-ui.theme.min.css') }}" rel="stylesheet">
+	<link href="{{ asset('statics/cms/css/jquery-ui/1.11.4/jquery-ui.structure.min.css') }}" rel="stylesheet">
 	<link href="{{ asset('statics/cms/css/bootstrap/3.3.6/bootstrap.min.css') }}" rel="stylesheet">
 	<link href="{{ asset('statics/cms/css/datatables/datatables.min.css') }}" rel="stylesheet">
 	<link href="{{ asset('statics/cms/css/dashboard.css') }}" rel="stylesheet">
 	<link href="{{ asset('statics/cms/css/cms.css') }}" rel="stylesheet">
 
 	<script src="{{ asset('statics/cms/js/jquery/2.1.4/jquery.min.js') }}"></script>
+	<script src="{{ asset('statics/cms/js/jquery-ui/1.11.4/jquery-ui.min.js') }}"></script>
 	<script src="{{ asset('statics/cms/js/bootstrap/3.3.6/bootstrap.min.js') }}"></script>
 	<script src="{{ asset('statics/cms/js/datatables/datatables.min.js') }}"></script>
+	<script src="{{ asset('statics/cms/js/plugins/gv-scrollup.js') }}"></script>
+	<script>
+		$(document).ready(function(){
+			$(this).addScrollup({scrollImg: "{{ asset('statics/cms/images/scrollup.png') }}"});
+		});
+	</script>
 </head>
 
 <body>
--- resources/views/cms/regions/edit.blade.php
+++ resources/views/cms/regions/edit.blade.php
@@ -13,26 +13,35 @@
 		</div>
 		@endif
 	</div>
-	<div class="cms-content">
-		<div class="regions-selected-countries">
-			@foreach($countries as $country)
-				@if($filtered_country_list->contains($country->code))
-					<div id="countrySelected_{{ $country->code }}" class="regions-selected-country btn btn-primary">{{ $country->name }}<span class="glyphicon glyphicon-remove"></span></div>
-				@endif
-			@endforeach
-		</div>
+	<div class="cms-content region-form-content">
 		{!! Form::open(['route' => ['regions.update', $region->id]]) !!}
 
 			<div class="form-group">
-				{!! Form::label('name', 'Name') !!}
 				{!! Form::text('name', $region->name, array('class' => 'form-control')) !!}
 			</div>
 
-			<div class="form-group">
+			<div class="regions-selected-countries">
 				@foreach($countries as $country)
-					<div class="country_item">
-						{!! Form::label($country->name, $country->name) !!}
-						{!! Form::checkbox('countries[]', $country->code, $filtered_country_list->contains($country->code), ['class' => 'field', 'id' => 'country_'.$country->code]) !!}
+					@if($filtered_country_list->contains($country->code))
+						<div id="countrySelected_{{ $country->code }}" class="regions-selected-country btn btn-primary">{{ $country->name }}<span class="glyphicon glyphicon-remove"></span></div>
+					@endif
+				@endforeach
+			</div>
+
+			<div class="form-group">
+				@foreach($countries_ordered_list as $letter => $ordered_countries)
+					<div class="region-country-group">
+						<span><h3>{{ $letter }}</h3></span>
+						@foreach($ordered_countries as $country)
+							<div class="country_item">
+								{!! Form::checkbox('countries[]', $country->code, $filtered_country_list->contains($country->code), ['class' => 'field', 'id' => 'country_'.$country->code]) !!}
+								@if($filtered_country_list->contains($country->code))
+									{!! Form::label($country->name, $country->name, array('class' => 'label-bold')) !!}
+								@else
+									{!! Form::label($country->name, $country->name) !!}
+								@endif
+							</div>
+						@endforeach
 					</div>
 				@endforeach
 			</div>
@@ -43,34 +52,56 @@
 
 		<script type="text/javascript">
 			$(document).ready(function(){
+				$(window).scroll(function() {
+					if($(this).scrollTop() >= 100){
+						$('.regions-selected-countries').addClass('regions-selected-countries-fixed');
+					}else{
+						$('.regions-selected-countries').removeClass('regions-selected-countries-fixed');
+					}
+				});
+
+				$('.region-form-content [name="name"]').focus();
+
 				$('.country_item input').on('click', function(e){
 					e.stopPropagation();
-					//e.preventDefault();
 
 					var country_code = $(this).prop('id').split('_');
 					country_code = country_code[1];
 
-					var country_name = $(this).prev().html();
+					var country_name = $(this).next().html();
 
 					if($(this).prop('checked')){
-						$('.regions-selected-countries').prepend('<div class="regions-selected-country btn btn-primary" id="countrySelected_'+country_code+'">'+country_name+'<span class="glyphicon glyphicon-remove"></span></div>');
+						var newItem = $('<div class="regions-selected-country btn btn-primary regions-selected-country-new" id="countrySelected_'+country_code+'">'+country_name+'<span class="glyphicon glyphicon-remove"></span></div>').hide().fadeIn(500, function(){ $(this).addClass('regions-selected-country-transition').removeClass('regions-selected-country-new'); });
+						$('.regions-selected-countries').append(newItem);
+						$(this).next().addClass('label-bold');
 					}else{
-						$('.regions-selected-countries #countrySelected_'+country_code).remove();
+						$('.regions-selected-countries #countrySelected_'+country_code).css({'backgroundColor' : '#c9302c', 'borderColor' : '#ac2925'}).fadeOut(500, function(){
+							$(this).remove();
+						});
+						$('#country_'+country_code).next().removeClass('label-bold');
 					}
 				});
 
 				$('.country_item').on('click', function(){
+
+
 					var country_code = $(this).find('input').prop('id').split('_');
 					country_code = country_code[1];
 
 					var country_name = $(this).find('label').html();
 
-					if($(this).children(":last").prop('checked')){
-						$(this).children(":last").prop('checked', false);
-						$('.regions-selected-countries #countrySelected_'+country_code).remove();
+					if($(this).children(":first").prop('checked')){
+						$(this).children(":first").prop('checked', false);
+						$('.regions-selected-countries #countrySelected_'+country_code).css({'backgroundColor' : '#c9302c', 'borderColor' : '#ac2925'}).fadeOut(500, function(){
+							$(this).remove();
+						});
+						$('#country_'+country_code).next().removeClass('label-bold');
 					}else{
-						$(this).children(":last").prop('checked', true);
-						$('.regions-selected-countries').prepend('<div class="regions-selected-country btn btn-primary" id="countrySelected_'+country_code+'">'+country_name+'<span class="glyphicon glyphicon-remove"></span></div>');
+						$(this).children(":first").prop('checked', true);
+
+						var newItem = $('<div class="regions-selected-country btn btn-primary regions-selected-country-new" id="countrySelected_'+country_code+'">'+country_name+'<span class="glyphicon glyphicon-remove"></span></div>').hide().fadeIn(500, function(){ $(this).addClass('regions-selected-country-transition').removeClass('regions-selected-country-new'); });
+						$('.regions-selected-countries').append(newItem);
+						$('#country_'+country_code).next().addClass('label-bold');
 					}
 				});
 
@@ -78,7 +109,9 @@
 					var country = $(this).prop('id').split('_');
 					country = country[1];
 					$('.country_item #country_'+country).prop('checked', false);
-					this.remove();
+					$(this).css({'backgroundColor' : '#c9302c', 'borderColor' : '#ac2925'}).fadeOut(500, function(){
+						$(this).remove();
+					});
 				});
 			});
 		</script>
